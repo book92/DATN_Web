9eaeb40e5cfded46bcec61877974483237352ab7
Your Name <you@example.com>
2024-11-20 16:14:12 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
+import Customer from './screens/Customer';
+import Device from './screens/Device';
+import Error from './screens/Error';
+import Settings from './screens/Settings';
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/customer" element={<Customer />} />
+        <Route path="/device" element={<Device />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
